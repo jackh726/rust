c639a50d4f98cf73464bf5bd2842ade24e0a5c8e
Jack Huey <jack.huey@umassmed.edu>
2020-06-01 19:23:23 -0400

    Add u32 for bound variables to Binder

--- compiler/rustc_middle/src/ty/mod.rs
+++ compiler/rustc_middle/src/ty/mod.rs
@@ -625,7 +625,7 @@ impl<'tcx> TyS<'tcx> {
 
 // `TyS` is used a lot. Make sure it doesn't unintentionally get bigger.
 #[cfg(target_arch = "x86_64")]
-static_assert_size!(TyS<'_>, 32);
+static_assert_size!(TyS<'_>, 40);
 
 impl<'tcx> Ord for TyS<'tcx> {
     fn cmp(&self, other: &TyS<'tcx>) -> Ordering {
@@ -1072,7 +1072,7 @@ crate struct PredicateInner<'tcx> {
 }
 
 #[cfg(target_arch = "x86_64")]
-static_assert_size!(PredicateInner<'_>, 48);
+static_assert_size!(PredicateInner<'_>, 56);
 
 #[derive(Clone, Copy, Lift)]
 pub struct Predicate<'tcx> {
