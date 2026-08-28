46981d00c4a78a69f9e0857675b6d42140d522b7
jackh726 <git@jackhuey.me>
2026-08-28 04:17:57 +0000

    Basics needed for deferred liveness

--- compiler/rustc_borrowck/src/type_check/liveness/trace.rs
+++ compiler/rustc_borrowck/src/type_check/liveness/trace.rs
@@ -19,7 +19,7 @@ use rustc_trait_selection::traits::query::dropck_outlives;
 use rustc_trait_selection::traits::query::type_op::{DropckOutlives, TypeOpOutput};
 use tracing::debug;
 
-use crate::polonius::record_live_region_variance;
+use crate::polonius::{DeferredLocals, record_live_region_variance};
 use crate::region_infer::values;
 use crate::type_check::liveness::local_use_map::LocalUseMap;
 use crate::type_check::{NormalizeLocation, TypeChecker};
@@ -45,7 +45,7 @@ pub(super) fn trace<'tcx>(
     move_data: &MoveData<'tcx>,
     relevant_live_locals: &[Local],
     boring_locals: &[Local],
-) {
+) -> (DeferredLocals<'tcx>, LocalUseMap) {
     let _timer = typeck.tcx().prof.generic_activity("borrowck_liveness_trace");
 
     let local_use_map = LocalUseMap::build(&relevant_live_locals, location_map, typeck.body);
@@ -58,11 +58,15 @@ pub(super) fn trace<'tcx>(
     );
     let mut results = LivenessResults::new(typeck, calc);
 
+    let deferred_locals = DeferredLocals::default();
+
     results.add_extra_drop_facts(relevant_live_locals);
 
     results.compute_for_all_locals(relevant_live_locals);
 
     results.dropck_boring_locals(boring_locals);
+
+    (deferred_locals, local_use_map)
 }
 
 pub(crate) struct LivenessCalculation<'a, 'tcx> {
